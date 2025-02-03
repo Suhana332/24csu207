6f88e391cc86a4c5883cb2ecddc3d06b6da88e5a
Suhana332 <shoryasindwani345@gmail.com>
2025-02-03 13:41:14 +0530

    Add files via upload

---
+++ cpp/assi_1_q2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1,n2,s;
+    cout << "enter first number:";
+    cin >> n1;
+    cout << " enter second number:";
+    cin >> n2;
+    s=n1+n2;
+    cout << "sum is:" << s;
+    return 0;
+}
