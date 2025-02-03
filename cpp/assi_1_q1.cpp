053e90fdffb008937266de614947cc9a88c18f55
Suhana332 <shoryasindwani345@gmail.com>
2025-02-03 13:32:33 +0530

    Add files via upload

---
+++ cpp/assi_1_q1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ char name[100]; 
+ int age;
+ cout << "Enter your name: ";
+ cin >> name;
+ cout << "Enter your age: ";
+ cin >> age;
+ cout << "Hello " << name << ", you are " << age << " years old!" << endl;
+return 0;
+}
